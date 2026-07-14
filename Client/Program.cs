bbb633ab302f4fadae6c75ea138c3160de708411
pyankov5525 <pyankov5525@users.noreply.github.com>
2026-07-14 09:02:22 +0000

    Исправление CS0246: добавлен using AccessibilityMap в Client/Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AccessibilityMap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
